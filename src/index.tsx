3d02a8f412ebdbc40694ebd0a3b822699383213b
BowangLan <60339191+BowangLan@users.noreply.github.com>
2022-11-19 11:29:54 -0800

    Revert "added react router"
    This reverts commit 46715590f945181c5592517f78028f954b9ba6e6.

--- src/index.tsx
+++ src/index.tsx
@@ -3,26 +3,16 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import HomePage from "./pages/HomePage";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "/",
-        element: <HomePage />,
-      },
-    ],
-  },
-]);
+	{ path: '/', element: <App /> }
+])
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+	document.getElementById("root") as HTMLElement
 );
 root.render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
+	<StrictMode>
+		<RouterProvider router={router} />
+	</StrictMode>
 );
