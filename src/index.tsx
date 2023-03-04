a9fab11c78d714d6731f53119380d7fd50d08fbf
BowangLan <60339191+BowangLan@users.noreply.github.com>
2023-03-04 10:57:19 -0800

    added header background

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,9 @@
 import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
 import {
   createBrowserRouter,
   RouterProvider,
-  Route,
 } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 
